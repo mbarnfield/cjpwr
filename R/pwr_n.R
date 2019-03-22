b36dbe3ccea5aafc45f6290d69b8e20244d70c81
Matthew Barnfield <m.barnfield@qmul.ac.uk>
2019-03-22 17:20:37 +0000

    add pwr_n

--- R/pwr_n.R
+++ R/pwr_n.R
@@ -5,6 +5,7 @@
 #' @param a The number of alternatives per choice task (2 in most designs).
 #' @param c The number of analysis cells - equal to largest number of possible levels for any one feature, or the largest product of levels of any two attributes for power of two-way interaction estimates (Johnson and Orme, 2003).
 #' @details \code{pwr_n} calculates the sample size needed for a conjoint design to be sufficiently powered, given the number of choice tasks, profiles and maximum number of levels per feature.
+#' @export
 #' @examples
 #' #conjoint design with five choice tasks per respondent, two alternative profiles per task, and maximum six levels per feature
 #' pwr_n(t = 5, a = 2, c = 6)
@@ -16,16 +17,16 @@
 #' pwr_n(c = 18, a = 2, t = 5)
 
 
-pwr_n <- 
+pwr_n <-
   function(t, a, c) {
     ta <- t * a
     min_c <- 500 * c
     max_c <- 1000 * c
     min_n <- min_c / ta
     max_n <- max_c /ta
-    out1 <- paste("sample size needed for minimum threshold >", 
+    out1 <- paste("sample size needed for minimum threshold >",
                   min_n)
-    out2 <- paste("much better sample size would be >", 
+    out2 <- paste("much better sample size would be >",
                   max_n)
     print(out1)
     print(out2)
