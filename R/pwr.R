70ca5e12a99c3bf0242e972fbae9d75923a0c51c
Matthew Barnfield <m.barnfield@qmul.ac.uk>
2019-03-25 16:53:08 +0000

    cat() instead of print()

--- R/pwr.R
+++ R/pwr.R
@@ -1,8 +1,5 @@
 #' @name cjpwr
-
-NULL
-
-
+#'
 #' @rdname cjpwr
 #' @title Simple Power Analysis for Conjoint Designs
 #' @description Johnson's rule-of-thumb calculation for determining power of conjoint designs.
@@ -10,7 +7,7 @@ NULL
 #' @param t The number of choice-tasks per respondent.
 #' @param a The number of alternatives per choice task (2 in most designs).
 #' @param c The number of analysis cells - equal to largest number of possible levels for any one feature, or the largest product of levels of any two attributes for power of two-way interaction estimates (Johnson and Orme, 2003).
-#' @details \code{pwr} divides the product of n, t, and a by c, to give Johnson's rule-of-thumb estimation of conjoint design power. It returns the result of this calculation and statements of whether this exceeds the minimal minimum threshold (500) and ideal minimum threshold (1000).
+#' @details \code{pwr} divides the product of n, t, and a by c, to give Johnson's rule-of-thumb estimation of conjoint design power. It returns a dataframe containing the inputs and result of this calculation, whether (yes/no) this exceeds the minimal minimum threshold (500) and ideal minimum threshold (1000), and and the necessary sample sizes (rounded up) necessary for minimum and ideal power thresholds.
 #' @export
 #' @examples
 #' #conjoint design with five choice tasks per respondent, two alternative profiles per task, and maximum six levels per feature
@@ -23,18 +20,35 @@ NULL
 #' pwr(c = 18, n = 1000, a = 2, t = 5)
 #' @seealso \code{\link{pwr_n}}
 
-pwr <-
-  function(n, t, a, c) {
+cjpwr <-
+  function(n, t, a = 2, c) {
+
     nta <- n*t*a
+
+    #calculate figure to be compared against 500 and 1000
     design_rep <- nta/c
-    out1 <- paste("nta/c =", design_rep, "\n")
-    out2 <- ifelse(design_rep >= 500,
-                   "sufficient power for minimum threshold\n",
-                   "insufficient power for minimum threshold\n")
-    out3 <- ifelse(design_rep >= 1000,
-                   "sufficient power for ideal threshold\n",
-                   "insufficient power for ideal threshold\n")
-    cat(out1)
-    cat(out2)
-    cat(out3)
+
+    #set up calculation to check for necessary n
+    ta <- t * a
+
+    min_c <- 500 * c
+
+    max_c <- 1000 * c
+
+    #calculate minimum sample size, rounded up, for minimal threshold
+    min_n <- ceiling(min_c / ta)
+
+    #calculate minimum sample size, rounded up, for ideal threshold
+    max_n <- ceiling(max_c / ta)
+
+    #return dataframe of results
+    data.frame(n = n,
+               t = t,
+               a = a,
+               c = c,
+               nta_c = design_rep,
+               minimum_sufficient = ifelse(design_rep >= 500, "yes", "no"),
+               ideal_sufficient = ifelse(design_rep >= 1000, "yes", "no"),
+               minimum_n = min_n,
+               ideal_n = max_n)
   }
--- README.md
+++ README.md
@@ -1,11 +1,7 @@
 # cjpwr
-`cjpwr` includes two very simple functions:  
+`cjpwr` carries out a very simple power analysis for conjoint designs.   
 
-- `pwr()` carries out a simple power analysis for conjoint designs.   
-- `pwr_n()` tells you what sample size you need, given your design, to have acceptable power.
 
-`pwr()` takes four arguments: `n` (sample size), `t` (number of choice tasks per respondent), `a` (number of alternatives per choice task), and `c` (number of analysis cells - equal to largest number of possible levels for any one feature, or the largest product of levels of any two attributes for power of two-way interaction estimates). It simply divides `t*n*a` by `c`. The output of `pwr` includes the result of this calculation, and two statements telling you whether your design exceeds the minimal minimum threshold (500) and the ideal minimum threshold (1000). 
-
-`pwr_n()` takes only `t`, `a`, and `c`. It returns two `n` values - one for the minimal and one for the ideal threshold.
+`pwr()` takes four arguments: `n` (sample size), `t` (number of choice tasks per respondent), `a` (number of alternatives per choice task), and `c` (number of analysis cells - equal to largest number of possible levels for any one feature, or the largest product of levels of any two attributes for power of two-way interaction estimates). It simply divides `t*n*a` by `c`. The output of `pwr` is a dataframe including the inputs and result of this calculation, whether (yes/no) your design exceeds the minimal minimum threshold (500) and the ideal minimum threshold (1000), and the necessary sample sizes to exceed these thresholds. 
 
 The calculation is based on recommendations from Orme (2010) and Johnson and Orme (2003).
