09b4a6048188687d4e9606223921c658f0a2d879
Matthew Barnfield <m.barnfield@qmul.ac.uk>
2019-03-28 09:39:52 +0000

    remove need to supply contest_no and profile_no

--- R/cjpwr_data.R
+++ R/cjpwr_data.R
@@ -4,9 +4,7 @@
 #' @param data A tidy, long-formate conjoint dataframe.
 #' @param formula A formula like those passed to cj/amce/mm functions in `cregg`. RHS variables are used to determine max feature levels.
 #' @param id A variable, within data, containing respondent IDs. Must be numeric.
-#' @param contest_no A variable, within data, containing contest/choice-task numbers. Must be numeric.
-#' @param profile_no A variable, within data, containing within-contest/choice-task profile numbers. Must be numeric.
-#' @details \code{cjpwr_data} finds and calculates n, t, a, and c from a tidy conjoint data input (with certain variables specified) and divides the product of n, t, and a by c, to give Johnson's rule-of-thumb estimation of conjoint design power. It returns a dataframe containing the inputs and result of this calculation, whether (yes/no) this exceeds the minimal minimum threshold (500) and ideal minimum threshold (1000), and the sample sizes (rounded up) necessary for minimum and ideal power thresholds. {cjpwr_data} uses features of tidyeval which mean variable names can be specified without quoting ("") and without referring back to the dataframe every time (via data$).
+#' @details \code{cjpwr_data} finds and calculates n, t, a, and c from a tidy conjoint data input (with a formula and id, similar to other tidy conjoint analyses) and divides the product of n, t, and a by c, to give Johnson's rule-of-thumb estimation of conjoint design power. It returns a dataframe containing the inputs and result of this calculation, whether (yes/no) this exceeds the minimal minimum threshold (500) and ideal minimum threshold (1000), and the sample sizes (rounded up) necessary for minimum and ideal power thresholds. {cjpwr_data} uses features of tidyeval which mean variable names can be specified without quoting ("") and without referring back to the dataframe every time (via data$).
 #' @export
 #' @examples
 #' #load example datasets from {cregg} (Leeper 2019)
@@ -18,20 +16,11 @@
 #' f1 <- ChosenImmigrant ~ Gender + Education + LanguageSkills +
 #' CountryOfOrigin + Job + JobExperience + JobPlans +
 #' ReasonForApplication + PriorEntry
-#' cjpwr_data(immigration, f1, CaseID, profile, contest_no)
-#' ##for taxes dataset, need to create contest_no and profile_no vars
-#' #work out how many contests/tasks per respondent
-#' tasks_taxes <- sum(taxes$chose_plan)/length(unique(taxes$ID))
-#' #work out number of profiles per task
-#' profiles_taxes <- nrow(taxes$ID)/length(unique(taxes$ID))
-#' profiles_tasks <- profiles_taxes/tasks_taxes
-#' #create profile_no and contest_no based on values of profiles_tasks and tasks_taxes
-#' taxes$profile_no <- rep(c(1,2))
-#' taxes$contest_no <- rep(c(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8))
-#' #run cjpwr_data for power analysis
+#' cjpwr_data(immigration, f1, CaseID)
+#' #same in taxes dataset but without pre-specified formula
 #' cjpwr_data(taxes, chose_plan ~ taxrate1 + taxrate2 + taxrate3 +
 #' taxrate4 + taxrate5 + taxrate6 + taxrev, ID, profile_no, contest_no)
-#' #include interaction variable for a pair of features
+#' ## include interaction variable for a pair of features
 #' library(tidyverse)
 #' immigration$ints <- interaction(immigration$Gender, immigration$PriorEntry)
 #' #specify new formula including interaction feature
@@ -39,31 +28,57 @@
 #' CountryOfOrigin + Job + JobExperience + JobPlans +
 #' ReasonForApplication + PriorEntry + ints
 #' #then just run cjpwr_data with the new formula
-#' cjpwr_data(immigration, f2, CaseID, profile, contest_no)
+#' cjpwr_data(immigration, f2, CaseID)
 
 
 
-cjpwr_data <- function(data, formula, id, contest_no, profile_no) {
+cjpwr_data <- function(data, formula, id) {
 
   #check tidyverse installed and load if so
   library("tidyverse")
 
-  #find arguments in data and calculate n, t, and a
+  #find id arg in data
   id_enq <- enquo(id)
-  n <- data %>%
-    dplyr::select(!!id_enq) %>%
+
+  #extract id column
+  id_df <- data %>%
+    dplyr::select(!!id_enq)
+
+  #number of respondents
+  id_unique <- id_df %>%
     unique() %>%
     nrow()
 
-  contest_enq <- enquo(contest_no)
-  t <- data %>%
-    dplyr::select(!!contest_enq) %>%
-    max()
+  #find outcome variable in lhs formula
+  outcome <- all.vars(stats::update(formula, . ~ 0))
+  if (!length(outcome) || outcome == ".") {
+    stop("'formula' is missing a left-hand outcome variable")
+  }
+  outcome_enq <- enquo(outcome)
 
-  pro_enq <- enquo(profile_no)
-  a <- data %>%
-    dplyr::select(!!pro_enq) %>%
-    max()
+  #extract outcome column from data
+  outcome <- data %>%
+    dplyr::select(!!outcome)
+
+  #total number of tasks
+  tasks <- sum(outcome)
+
+  #per respondent
+  choices <- tasks/id_unique
+
+  #number of profiles
+  profiles <- nrow(id_df)
+
+  #per respondent
+  profiles_resp <- profiles/id_unique
+
+  #profiles per task
+  profiles_task <- profiles_resp/choices
+
+  #n, t, a
+  n <- id_unique
+  t <- choices
+  a <- profiles_task
 
   #calculate c
   features <- all.vars(stats::update(formula, 0 ~ .))
@@ -72,6 +87,7 @@ cjpwr_data <- function(data, formula, id, contest_no, profile_no) {
   for (i in seq_along(features_df)) {
     lengths[[i]] <- length(unique(features_df[[i]]))
   }
+
   c <- max(lengths)
 
 
@@ -105,8 +121,3 @@ cjpwr_data <- function(data, formula, id, contest_no, profile_no) {
              minimum_n = min_n,
              ideal_n = max_n)
 }
-
-
-
-
-
